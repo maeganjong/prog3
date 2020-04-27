61fa0c6906aa139563c2bc35b0bfb2beba5e67a1
Maegan Jong <maegan.jong@gmail.com>
2020-04-26 22:26:27 -0500

    Write.java edits and test files

--- write.java
+++ write.java
@@ -3,32 +3,34 @@ import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class write{
-  final static int N = 100;
   public static void main(String[] args) {
-    try {
-      File myObj = new File(N + ".txt");
-      if (myObj.createNewFile()) {
-        System.out.println("File created: " + myObj.getName());
-      } else {
-        System.out.println("File already exists.");
+    for (int i = 1; i <= 100; i++){
+      try {
+        File myObj = new File(i + ".txt");
+        if (myObj.createNewFile()) {
+          System.out.println("File created: " + myObj.getName());
+        } else {
+          System.out.println("File already exists.");
+        }
+      } catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
       }
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+
+      try {
+        BufferedWriter myWriter = new BufferedWriter(new FileWriter(i + ".txt"));
+
+        for(int j = 0; j < 100; j++){
+          myWriter.write(Long.toString(ThreadLocalRandom.current().nextLong((long) Math.pow(10,12)) + 1) + "\n");
+          // myWriter.write(ThreadLocalRandom.current().nextLong((long) Math.pow(10,12)) + 1);
+        }
+        myWriter.close();
+        System.out.println("Successfully wrote to the file.");
+      } catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+      }
     }
 
-    try {
-      BufferedWriter myWriter = new BufferedWriter(new FileWriter(N + ".txt"));
-
-      for(int i = 0; i < N; i++){
-        myWriter.write(Long.toString(ThreadLocalRandom.current().nextLong((long) Math.pow(10,12)) + 1) + "\n");
-        // myWriter.write(ThreadLocalRandom.current().nextLong((long) Math.pow(10,12)) + 1);
-      }
-      myWriter.close();
-      System.out.println("Successfully wrote to the file.");
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
   }
 }
